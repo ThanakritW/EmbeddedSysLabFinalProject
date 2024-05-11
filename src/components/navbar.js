d2283726ff388f7ec2c6368b3ea69849c74718b8
Thanakrit Wongasa <gain_thanakrit@kkumail.com>
2024-05-11 14:28:02 +0700

    feat: sticky navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import { CurrentTime } from "./currentTime";
 
 export function Navbar() {
     return (
-        <div className="navbar">
+        <div className="navbar" style={{ position: "sticky", top: "0" }}>
             <h1 className="navbar-title">Embedded System Laboratory</h1>
             <div className="navbar-items">
                 <CurrentTime />
