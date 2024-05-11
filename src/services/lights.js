113db2799ffd856746621d51436bfc62b2c10d15
Thanakrit Wongasa <gain_thanakrit@kkumail.com>
2024-05-11 15:06:56 +0700

    fix: increase data point
    So that data points span across the graph max axis

--- src/services/lights.js
+++ src/services/lights.js
@@ -12,13 +12,13 @@ export const getLight = async () => {
   const lightRef = collection(db, "lights");
 
   const currentTime = new Date();
-  const TIME_RANGE = 5 * 60 * 1000; // 5 minutes
+  const TIME_RANGE = ((5 * 60) + 10) * 1000; // 5 minutes + 10 sec for delay 
   const minRangeTime = new Date(currentTime.getTime() - TIME_RANGE);
   const q = query(
     lightRef,
     where("time", ">=", minRangeTime),
     orderBy("time", "desc"),
-    limit(12)
+    limit(13) //so that data point cover all 0-60 in 5 sec interval
   );
   const querySnapshot = await getDocs(q);
 
--- src/services/passage.js
+++ src/services/passage.js
@@ -12,13 +12,13 @@ export const getPassage = async () => {
   const passageRef = collection(db, "passage");
 
   const currentTime = new Date();
-  const TIME_RANGE = 60 * 60 * 1000; // 60 minutes
+  const TIME_RANGE = 61 * 60 * 1000; // 61 minutes (1 minutes for delay)
   const minRangeTime = new Date(currentTime.getTime() - TIME_RANGE);
   const q = query(
     passageRef,
     where("time", ">=", minRangeTime),
     orderBy("time", "desc"),
-    limit(60)
+    limit(61) // so that it span all 0-60 min with 1 min interval (61 data point)
   );
   const querySnapshot = await getDocs(q);
 
